dad682073c3baa965ef9274c97bbecf4fb70e923
Andrew <andrewbaker@live.com>
2023-07-05 23:56:44 +0100

    Added docstrings for competitions/apps.py

--- competitions/apps.py
+++ competitions/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
 
-
 class CompetitionsConfig(AppConfig):
+    """A class that configures the Competitions db model in Django
+
+    :param default_auto_field: defaults to `django.db.models.BigAutoField`
+    :type default_auto_field: str
+    :param name: the name of the model, defaults to `competitions`
+    :type name: str
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'competitions'
