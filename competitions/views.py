2c6998ff190cf33d9f3f8af27f9c779d894e59a8
Andrew <andrewbaker@live.com>
2023-07-06 00:23:42 +0100

    Added docstrings for competitions/views.py

--- competitions/views.py
+++ competitions/views.py
@@ -4,15 +4,40 @@ from datetime import datetime
 
 # Create your views here.
 def competition_page(request):
+    """Returns a rendered view of the requested competitions page
+    :param request: HTTP request initiated by the user
+    :type request: HTTP request
+    :return: The competition_page.html webpage containing a view of the competitions
+    :rtype: HTTP response
+    """
     competitions = Competition.objects.all().order_by("-date")[:25]
     context = {'competition_list': competitions}
     return render(request, 'competitions/competition_page.html', context)
 
 def view_competition(request, competition_id):
+    """Returns a rendered view of a page linked to a specific competition
+    :param request: HTTP request initiated by the user
+    :type request: HTTP request
+    :param competition_id: The identifier for the selected competition (primary key, title)
+    :type competition_id: str
+    :return: The competition.html webpage containing a view of the specific competition
+    :rtype: HTTP response
+    """
     competition = get_object_or_404(Competition, pk=competition_id)
     return render(request, 'competitions/competition.html', {'competition': competition})
 
 def enter(request, competition_id):
+    """Returns a success message if the user successfully enters the competition, or an error
+    message asking the user to log in if they are not authenticated
+    :param request: HTTP request initiated by the user
+    :type request: HTTP request
+    :param competition_id: The identifier for the selected competition (primary key, title)
+    :type competition_id: str
+    :return: The post_entry.html page with a success message if the user is authenticated and
+    successfully enters, or the membership/login.html page with an error message if the user
+    is not logged in and authenticated
+    :rtype: HTTP response
+    """
     competition = get_object_or_404(Competition, pk=competition_id)
     
     if request.user.is_authenticated:
