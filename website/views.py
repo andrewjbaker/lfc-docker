8309ab9355a7548175f50ab4ca888b29efb290e7
Andrew <andrewbaker@live.com>
2023-07-06 00:56:10 +0100

    Added docstrings for website/views.py

--- website/views.py
+++ website/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    """Returns a rendered view of the website home page
+    :param request: HTTP request initiated by the user
+    :type request: HTTP request
+    :return: The website home page, index.html
+    :rtype: HTTP response
+    """
     return render(request, "index.html")
 
 def honours(request):
+    """Returns a rendered view of the requested club honours page
+    :param request: HTTP request initiated by the user
+    :type request: HTTP request
+    :return: The honours.html webpage containing the history of the club's trophy successes
+    :rtype: HTTP response
+    """
     return render(request, "honours.html")
